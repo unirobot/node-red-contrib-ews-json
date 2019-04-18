6dff2aa5fcdaf9ea00de8342354d3c9bd997bcc9
Hiroyuki Okada <okada@unirobot.com>
2019-04-18 18:48:07 +0900

    create ews folder

--- ews/ews.js
+++ ews/ews.js
@@ -1,26 +1,47 @@
 module.exports = function (RED) {
     "use strict";
     const EWS = require('node-ews');
+    const fs = require('fs');
 
     function EwsNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         this.on('input', function (msg) {
-            const ews = new EWS(msg.ewsConfig);
-
-            ews.run(msg.ewsFunction, msg.ewsArgs)
-                .then(result => {
-                    msg.ewsResult = result;
-                    msg.status = "success";
-                    node.send(msg);
-                })
-                .catch(err => {
-                    msg.payload = err.message;
-                    msg.status = "error";
-                    node.send(msg);
-                    console.log(err.stack);
-                });
+            createTempDir(node, msg);
         });
     }
+
+    function createTempDir(node, msg){
+        const dir = RED.settings.userDir + "/ews";
+        if(fs.existsSync(dir)){
+            msg.ewsConfig.temp = dir;
+            run(node, msg);
+        }else{
+            fs.mkdir(dir, (err) => {
+                if (!err){
+                    msg.ewsConfig.temp = dir;
+                }
+                run(node, msg);
+            });
+        }
+    }
+
+    function run(node, msg){
+        const ews = new EWS(msg.ewsConfig);
+
+        ews.run(msg.ewsFunction, msg.ewsArgs)
+            .then(result => {
+                msg.ewsResult = result;
+                msg.status = "success";
+                node.send(msg);
+            })
+            .catch(err => {
+                msg.payload = err.message;
+                msg.status = "error";
+                node.send(msg);
+                console.log(err.stack);
+            });
+    }
+
     RED.nodes.registerType("ews", EwsNode);
 };

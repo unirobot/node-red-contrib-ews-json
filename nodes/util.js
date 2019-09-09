705da4382934b4bfc887f665642f1a1ed5381011
Hiroyuki Okada <okada@unirobot.com>
2019-09-09 16:02:44 +0900

    add chown sync

--- nodes/util.js
+++ nodes/util.js
@@ -18,13 +18,19 @@ function deleteDir(dir){
     }
 }
 
+function changeOwnerToSystem(dir){
+    fs.chownSync(dir, 1000, 1000);
+}
+
 function createTempDir(dir, msg){
     try{
         if(fs.existsSync(dir)){
+            changeOwnerToSystem(dir);
             msg.ewsConfig.temp = dir;
             return msg;
         }else{
             fs.mkdirSync(dir);
+            changeOwnerToSystem(dir);
             msg.ewsConfig.temp = dir;
             return msg;
         }
